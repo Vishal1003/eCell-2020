4e67b874fdacdfe81d710f9b34ee5e87a8cf9947
Vishal1003 <vjha10032001@gmail.com>
2021-05-17 20:25:09 +0530

    resolved dep issues

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(function (req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT http://localhost:${PORT}/`)
